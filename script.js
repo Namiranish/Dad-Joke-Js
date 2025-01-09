62761646c82216f4de7fdef457103743bc143559
Namirah anis <namiranish@gmail.com>
2025-01-09 19:45:13 +0530

    completed the task

---
+++ script.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById('btn')
+const display = document.querySelector('#joke')
+
+
+async function getJoke(){
+
+    let apiUrl = 'https://api.api-ninjas.com/v1/dadjokes'
+    
+    let header = {
+        headers:{
+            'X-Api-Key':'3LTl8wEySXfK+mHPXEuZUQ==XAnXoUxjbWCf3tOI'
+        }
+    }
+    try{
+    //1. fetch the response object
+    let response = await fetch(apiUrl,header);
+    //2.convert the response object to json(java script object notation)
+    let result = await response.json();
+
+    // console.log(result);
+    
+    display.innerText = `${result[0].joke}`
+    }
+    catch(error){
+        display.innerText = 'Error occured';
+    }
+
+
+}
+
+
+btn.addEventListener('click', getJoke)
+
